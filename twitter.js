f6e06d49ca20c91cf0fe3fcc07119853ebda2e66
Edwyn Guzman <edwynjesus30@gmail.com>
2022-12-14 23:01:17 -0400

    Optimizacion-base

--- twitter.js
+++ twitter.js
@@ -1,15 +1,9 @@
-
-const closeIcon = document.querySelector(".close");
 const loginSesion = document.querySelector(".subContenedor");
-const inputEmail = document.querySelector(".correoElectronico");
 
-
-    function loginClose(){
-        loginSesion.style.display = "none";
-        document.querySelector(".contenedor").style.backgroundColor ="#fff"
-    }
-
-closeIcon.addEventListener("click",loginClose)
+document.querySelector(".close").addEventListener("click",()=>{
+    loginSesion.style.display = "none";
+    document.querySelector(".contenedor").style.backgroundColor ="#fff"
+})
 
 
     
