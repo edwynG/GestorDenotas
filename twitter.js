de3b44d2e523cc5391ad82c443e33edeb9e86e30
Edwyn Guzman <edwynjesus30@gmail.com>
2023-01-27 20:21:00 -0400

    Fucntion fist ready

--- twitter.js
+++ twitter.js
@@ -5,13 +5,11 @@ const CI = document.getElementById("CI")
 const DivErrores = document.querySelector(".error")
 const  modalpersonal = document.querySelector(".modalpersonal")
 const modalMian = document.getElementById("modalMian")
-modalpersonal.style.display = "none";
+
 
 
 //funcion para validar si el dato es un numero entero
 const CorrectExpre = (e)=>{
- 
-
   let con = parseInt(e.target.value)
   if (Number.isInteger(con)) {
     console.log("Correcto")
@@ -42,18 +40,17 @@ const CorrectExpre = (e)=>{
 
 .then((a) => { 
   const datos = a;
-  console.log(a)
+  
 
-  ValidarCI.addEventListener("click",()=>{
+    ValidarCI.addEventListener("click",()=>{
     
-      for(let i = 0;i < datos.length;i++)
-      if ( CI.value == datos[i].ci) {
-        console.log( CI.value )
-        var resCi = datos[i].ci;
-        var datosPersona = datos[i]
-        
-        break;
+      for(let i = 0;i < datos.length;i++){ 
+        if ( CI.value == datos[i].ci) {
+          var resCi = datos[i].ci;
+          var datosPersona = datos[i]  
+        }
       }
+     
       if (CI.value != resCi){
         console.log("no se encontro")
         DivErrores.innerHTML = "Cedula de identidad no encontrada"
@@ -61,56 +58,60 @@ const CorrectExpre = (e)=>{
         
       }
       else{
-        console.log("buscando datos")
         DivErrores.setAttribute("hidden",true)
         modalMian.classList.add("removeModalMain")
         modalpersonal.style.display = "block";
         document.getElementById("nombre").innerHTML = datosPersona.nombre;
         document.getElementById("ciPersonal").innerHTML = datosPersona.ci;
         document.getElementById("carrera").innerHTML = datosPersona.carrera;
-        let materias = datosPersona.materias;
-        let notas = datosPersona.notas;
-        console.log(materias)
+        const materias = datosPersona.materias;
+        const notas = datosPersona.notas;
+        //tabla de Calificaciones
         let tablaMaterias =   document.querySelector(".tablaMaterias")
-        let tablaNotas =   document.querySelector(".tablaNotas")
-
-
-       for (let i = 0; i < materias.length; i++) {
-        let dateCase  = materias[i]
-        let div = document.createElement("DIV")
-        div.textContent = dateCase;
-        tablaMaterias.appendChild(div)
-        
-        
-       }
-
-       notas.forEach(element => {
-        let dateCase  = element
-        let div = document.createElement("div")
-        div.textContent = dateCase;
-        tablaNotas.appendChild(div)
-       });
-          
-         }
+        tablaNotas(materias,notas,tablaMaterias)
+        //Caja de Promedio
+        let resultadoPromedio = Promedio(notas)
+        let promedio = document.querySelector(".promedio")
+        promedio.innerHTML += `<div> ${resultadoPromedio.toFixed(2)}</div>`
+      }
       
-       })
-
-
-
-
-     })
+    })
+})
 //////////////////////////////////////////////////////////////////////
 
+// funcion para mostrar promedio
+const Promedio =(prome)=>{
+  let total = 0;
+  prome.forEach((e)=>{
+    total+=e
+  })
+  
+  let res = total / prome.length
 
+  return res
+}
+
+
+function tablaNotas(materias,notas,tablaMaterias) {
+  for (let i = 0; i < materias.length; i++) {
+    let dateCase  = materias[i]
+    let div = document.createElement("DIV")
+    div.textContent = dateCase;
+    div.classList.add("styleMateria")
+    tablaMaterias.appendChild(div)
+    
+
+    let notaCount = notas[i];
+    let div2 = document.createElement("DIV")
+    div2.textContent = notaCount;
+    div2.classList.add("styleNota")
+    tablaMaterias.appendChild(div2)
+   }
+}
 
 // Funcion para cerrar y abrir modal
 document.querySelector(".close").addEventListener("click",()=>{
-    loginSesion.style.display = "none";
-    document.querySelector(".contenedor").style.backgroundColor ="#fff"
+  loginSesion.style.display = "none";
+  document.querySelector(".contenedor").style.backgroundColor ="#fff"
 })
 /////////////////////////////////////////
-
-
-
-
-
